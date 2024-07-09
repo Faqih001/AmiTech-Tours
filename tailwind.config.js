87f028adcd4f9c4819af635f914d9cb4e55f3a5a
Amirul Faqih <fakiiahmad001@gmail.com>
2024-07-09 23:10:42 +0300

    chore: Update TailwindCSS configuration to include green and orange colors and add new boxShadow utility

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
       colors: {
         green: "#63Ab45",
         orange: "#F7921E",
+      },
+      boxShadow: {
+        "x1": "0px 10px 60px 0px rgba(0, 0, 0, 0.15)",
       }
     },
   },
