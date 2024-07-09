bdb180708fc48508fa47c7501406dabe9aaeae6f
Amirul Faqih <fakiiahmad001@gmail.com>
2024-07-09 23:08:00 +0300

    chore: Update TailwindCSS configuration to include green and orange colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: "#63Ab45",
+        orange: "#F7921E",
+      }
+    },
   },
   plugins: [],
 }
